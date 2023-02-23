08bde8046ff97d0557e45a7576010cb60dafe9b8
Mayor90 <108475752+Mayor90@users.noreply.github.com>
2023-02-23 12:52:50 -0500

    Delete WORDLE directory

--- WORDLE/testissues.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class testissues {
-    public static void main (String[] args){
-        String y = "GGGGG";
-        if (y == "GGGGG") System.out.println("Suck my Dick and Balls. I work at NASA");
-    
-    }
-}
